5f1565634ee574e5fb07a887d3eb2d4f19fb5c8c
gugu <lar542@naver.com>
2020-11-10 17:47:29 +0900

    메인 페이지

---
+++ src/views/MainPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="main list-container contents">
+      <ul>
+        <DakkuDiary></DakkuDiary>
+        <DakkuTools></DakkuTools>
+        <DakkuItems></DakkuItems>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import DakkuDiary from '@/components/common/dakku/DakkuCanvas.vue';
+import DakkuTools from '@/components/common/dakku/DakkuTools.vue';
+import DakkuItems from '@/components/common/dakku/DakkuItems.vue';
+
+export default {
+  components: {
+    DakkuDiary,
+    DakkuTools,
+    DakkuItems,
+  },
+};
+</script>
+
+<style></style>
